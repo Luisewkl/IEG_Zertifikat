a74414133844a9cd18dac5bbd27ce938e4419e28
Luisewkl <luise.winkeler@icloud.com>
2026-06-15 16:21:49 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -12,9 +12,9 @@
    8. Diese Datei speichern und auf GitHub hochladen
    ============================================ */
 
-const SUPABASE_URL = 'https://wjocijiccmbhdnopwnlg.supabase.co/rest/v1/
-';// z.B. 'https://abcdefghijk.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indqb2NpamljY21iaGRub3B3bmxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1Mjc5ODUsImV4cCI6MjA5NzEwMzk4NX0.MpVu6AnZBMZVo-YVboXVLgtc5RSRqrI-ylDAFpOfC8c'; // z.B. 'eyJhbGciOiJIUzI1NiIsInR5cCI6...'
+const SUPABASE_URL = 'https://wjocijiccmbhdnopwnlg.supabase.co/rest/v1/'
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indqb2NpamljY21iaGRub3B3bmxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1Mjc5ODUsImV4cCI6MjA5NzEwMzk4NX0.MpVu6AnZBMZVo-YVboXVLgtc5RSRqrI-ylDAFpOfC8c'
 
 /* ============================================
    SQL-CODE FÜR SUPABASE (einmalig ausführen):
