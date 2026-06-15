4e6cf24f253f361ea66992fa2b75fbce0ab04877
Luisewkl <luise.winkeler@icloud.com>
2026-06-15 16:59:43 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 /* IEG Claude Academy — Supabase Config */
 
 const SUPABASE_URL = 'https://wjocijiccmbhdnopwnlg.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_H3M2xQxgfN-BDSQWDevDgQ_E3SojvIi';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indqb2NpamljY21iaGRub3B3bmxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1Mjc5ODUsImV4cCI6MjA5NzEwMzk4NX0.MpVu6AnZBMZVo-YVboXVLgtc5RSRqrI-ylDAFpOfC8c';
 
 window._supabaseURL = SUPABASE_URL;
 window._supabaseKEY = SUPABASE_ANON_KEY;
