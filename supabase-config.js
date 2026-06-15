5d81d2d96b5b7b3e3dc9eadeee0bc70a26267b24
Luisewkl <luise.winkeler@icloud.com>
2026-06-15 17:14:49 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 /* IEG Claude Academy — Supabase Config */
 
-const SUPABASE_URL = 'https://wjocijiccmbhdnopwnlg.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indqb2NpamljY21iaGRub3B3bmxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1Mjc5ODUsImV4cCI6MjA5NzEwMzk4NX0.MpVu6AnZBMZVo-YVboXVLgtc5RSRqrI-ylDAFpOfC8c';
+const SUPABASE_URL = 'https://hojkbskyhwocsknucvos.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvamtic2t5aHdvY3NrbnVjdm9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MzI4NzYsImV4cCI6MjA5NzEwODg3Nn0.A2jd2EPn9bSBHiUh-CQDbx-zUnGart4iU688gXypT3c';
 
 window._supabaseURL = SUPABASE_URL;
 window._supabaseKEY = SUPABASE_ANON_KEY;
