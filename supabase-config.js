867425c51a892799045f6ea8aa82044ba25da0ab
Luisewkl <luise.winkeler@icloud.com>
2026-06-15 16:28:01 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -12,7 +12,7 @@
    8. Diese Datei speichern und auf GitHub hochladen
    ============================================ */
 
-const SUPABASE_URL = 'https://wjocijiccmbhdnopwnlg.supabase.co/rest/v1/'
+const SUPABASE_URL = 'https://wjocijiccmbhdnopwnlg.supabase.co'
 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indqb2NpamljY21iaGRub3B3bmxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1Mjc5ODUsImV4cCI6MjA5NzEwMzk4NX0.MpVu6AnZBMZVo-YVboXVLgtc5RSRqrI-ylDAFpOfC8c'
 
